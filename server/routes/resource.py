36f573fc5a6b795d60470e35936a91f0db32aaac
Jonathan <jonathanelian64@gmail.com>
2026-03-29 11:32:40 -0700

    feat: mejores nombres de atributos json recursos

--- server/routes/resource.py
+++ server/routes/resource.py
@@ -10,12 +10,12 @@ resource_router = Blueprint('resources', __name__, url_prefix='/resources')
 
 def recurso_to_dict(recurso: Recurso) -> dict:
     return {
-        "ID_Rcrs": str(recurso.ID_Rcrs),
-        "Link": recurso.Link,
-        "ID_Usr": str(recurso.ID_Usr),
-        "Dscrpcn": recurso.Dscrpcn,
-        "Fch_plcn": recurso.Fch_plcn.isoformat(),
-        "ID_pblcn": str(recurso.ID_pblcn) if recurso.ID_pblcn else None,
+        "id": str(recurso.ID_Rcrs),
+        "link": recurso.Link,
+        "id_usuario": str(recurso.ID_Usr),
+        "descripcion": recurso.Dscrpcn,
+        "fecha_creacion": recurso.Fch_plcn.isoformat(),
+        "id_publicacion": str(recurso.ID_pblcn) if recurso.ID_pblcn else None,
     }
 
 def get_all_recursos(db: Session) -> list[dict]:
